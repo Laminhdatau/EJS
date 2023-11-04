43c6b43561703d7908ae0bf5fede61e55cdfda5e
Laminhdatau <Laminhdataulamin2@gmail.com>
2023-11-04 11:32:35 +0800

    [+] Set path running all

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
 app.set("view engine", "ejs");
+
+//Agar bisa running dari mana saja
+app.set("views", path.join(__dirname, "/views"));
 
 app.get("/", (req, res) => {
   res.render("home.ejs");
